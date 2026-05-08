936ea9270c6e0df7489f7d6bbdf4d27cf3add6bd
prachipossesivepanda <prachi@possesivepanda.online>
2026-05-08 23:21:03 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -18,16 +18,7 @@ const allowedOrigins = [
 ];
 
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
+  cors()
 );
 
 app.use(express.json({ limit: "1mb" }));
